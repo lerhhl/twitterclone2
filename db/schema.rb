c2272e9a8a1c024d481a05a45356b8ddea9ba961
ahens31 <sneha.iyer05@gmail.com>
2017-05-30 17:46:29 +0800

    user model validations

--- db/schema.rb
+++ db/schema.rb
@@ -64,12 +64,12 @@ ActiveRecord::Schema.define(version: 20170530082123) do
     t.datetime "last_sign_in_at"
     t.inet "current_sign_in_ip"
     t.inet "last_sign_in_ip"
-    t.string "first_name", null: false
-    t.string "last_name", null: false
-    t.string "username", null: false
-    t.text "description", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "username"
+    t.text "description"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  it { should have_many(:tweets)}
+  it { should validate_presence_of(:first_name)}
+  it { should validate_presence_of(:last_name)}
+  it { should validate_presence_of(:username)}
+  it { should validate_length_of(:username).is_at_least(3)}
+
+  describe 'validates uniqueness of' do
+      subject {User.create(email: "example@example.com", password: "123123", first_name: "john", last_name: "doe", username: "johndoe")}
+      it { should validate_uniqueness_of (:username)}
+    end
+end
+
+# Test whether devise gem tests uniqueness of email and email validity (@ sign)
