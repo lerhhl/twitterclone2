380dc3c62ba7a110b2f54bf01b3b1f86d42154fc
ahens31 <sneha.iyer05@gmail.com>
2017-05-30 18:06:09 +0800

    Update schema

--- db/migrate/20170530074156_add_column_to_user.rb
+++
@@ -1,8 +0,0 @@
-class AddColumnToUser < ActiveRecord::Migration[5.1]
-  def change
-    add_column :users, :first_name, :string
-    add_column :users, :last_name, :string
-    add_column :users, :username, :string
-    add_column :users, :description, :text  
-  end
-end
