a8b77791e3f752beb386e8b765f877ce8feae218
lerhhl <lerhhl@gmail.com>
2017-05-30 18:10:08 +0800

    Create homepage, login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users, :controllers => { registrations: 'registrations' }
+
+  resources :users do
+    resources :tweets
+  end
+
+  root 'static_pages#home'
+
 end
