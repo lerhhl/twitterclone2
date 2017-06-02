d31e44559178c79aa0707035e7fce64389189138
ahens31 <sneha.iyer05@gmail.com>
2017-06-02 13:39:49 +0800

    model and controller specs

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -6,28 +6,29 @@ class TweetsController < ApplicationController
   # User needs to login before doing the following actions
   before_action :authenticate_user!, only: [:new, :create, :edit, :destroy]
 
-  
+
   def new
     @tweet = Tweet.new
   end
-  
+
   def create
-    @user = current_user
     @tweet = Tweet.new(tweet_params)
+    @tweet.user = current_user
+    #@tweet = current_user.tweets.build(tweet_params)
     if @tweet.save
       redirect_to show_path
     else
       render :new
-    end   
+    end
   end
-  
-  def edit 
+
+  def edit
   end
 
   def update
     @tweet.update(tweet_params)
     redirect_to show_path
-  end  
+  end
 
   def destroy
     @tweet.destroy
@@ -35,11 +36,11 @@ class TweetsController < ApplicationController
   end
 
   private
-    
+
   def tweet_params
     params.require(:tweet).permit(:body, :user_id)
   end
-  
+
   def set_tweet
     @tweet = Tweet.find(params[:id])
   end
